5636b3abbb1c19195043c91631653d1e8f84bafa
Daniel Marcovich <danielmark999@gmail.com>
2023-01-24 14:41:50 -0500

    feat(classes): general update

--- public/helpers/index.js
+++ public/helpers/index.js
@@ -2,16 +2,16 @@ import cts from "./constants.js"
 import createElement from "./createElement.js"
 import getData from "./getData.js"
 import getDate from "./getDate.js"
-import createUser from "./createUser.js"
-import getWeek from "./getWeek.js"
-import getTransactionDate from "./getTransactionDate.js"
+import getCurrentDate from "./getCurrentDate.js"
+import getUserDate from "./getUserDate.js"
+import userFilter from "./userFilter.js"
 
 export {
     cts,
     createElement,
     getData,
     getDate,
-    createUser,
-    getWeek,
-    getTransactionDate
+    getCurrentDate,
+    getUserDate,
+    userFilter
 }
